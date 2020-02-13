9366989d2f9bbf31260cace56660dbb5077b263e
Charles Pelton <charliejp0311@gmail.com>
2020-02-13 14:11:43 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,11 @@
 require "pry"
 class Student
-  attr_accessor :name
+  def hello
+    puts "Hey there! I'm so excited to learn stuff.\n"
+  end
+
+  def raise_hand
+    puts "Pick me!\n"
+  end
 
 end
