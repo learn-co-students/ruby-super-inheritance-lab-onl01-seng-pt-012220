0a94e247e4bfbe00e46edc4948af2a5554811d26
Xavier Perez <xperez007@gmail.com>
2020-02-22 18:14:15 +0000

    Done.

---
+++ lib/chatty_student.rb
@@ -0,0 +1,17 @@
+require_relative "../lib/student.rb"
+require_relative "../lib/chatty_student.rb"
+
+class chatty_student
+  
+def hello :student, :class
+  puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
+end
+
+def raise_hand(super{10})
+  puts out "Pick me!"
+  
+end
+
+
+end
+
---
+++ lib/student.rb
@@ -0,0 +1,16 @@
+require_relative "../lib/student.rb"
+require_relative "../lib/chatty_student.rb"
+
+class student
+
+def hello
+  puts "Hey there! I'm so excited to learn stuff."
+end
+
+def raise_hand
+  puts "Pick me!"
+end
+
+
+  
+end
